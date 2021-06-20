612c7675ce4e2089a65b472e4f57567fabf5e610
Sai <snaadira@gmail.com>
2021-06-20 09:16:44 -0400

    added prepend and append methods

--- ds-algorithms/linked-list-notes.js
+++ ds-algorithms/linked-list-notes.js
@@ -14,23 +14,38 @@
 
 
 // example
-//10 --> 5 --> 16 --> null
+//1--> 10 --> 99 --> 5 --> 16 --> null
 
-let myLinkedList = {
-  // first item in linked list
-  head: {
-    value: 10,
-    next: {
-      value: 5,
-      next: {
-        value: 16,
-        next: null
-      }
-    }
-  }
-}
+// let myLinkedList = {
+//   // first item in linked list
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null
+//       }
+//     }
+//   }
+// }
 
-// creating a class for above code
+
+// ^^^ creating a class for above code ^^
+
+
+// to keep code dry you can use a node class
+// // that creates node for you and instantiate that class
+// class Node {
+//   constructor(value) {
+//     this.value = value
+//     this.next = null
+//   }
+// }
+// use it like this: 
+// const newNode = new Node(value)
+
+
 class LinkedList {
   // creates head or very first node
   constructor(value) {
@@ -55,17 +70,62 @@ class LinkedList {
     this.length++
     return this
   }
+  prepend(value) {
+    // creating an empty node with the value
+    const newNode = {
+      value: value,
+      next: null
+    }
+    // new node with pointer to next node is now the head node
+    newNode.next = this.head
+    // the head is equal to newly created node (firstNode)
+    this.head = newNode
+    // increment length by one
+    this.length++
+    // return linked list
+    return this
+  }
+
+  printList() {
+    const array = []
+    // current node is the head node in the list
+    let currentNode = this.head
+    //while loop: while condition is happening, run below commands
+    // as long as there is a current node that doesnt point to null
+    while (currentNode !== null) {
+      // add to array the value of the current node
+      array.push(currentNode.value)
+      // then update the current node to equal next until null is reached
+      currentNode = currentNode.next
+    }
+    return array
+  }
+  // insert(index, value) {
+
+  // }
 }
 
 const myLinkedlist = new LinkedList(10)
 myLinkedlist.append(5)
 myLinkedlist.append(16)
-console.log(myLinkedlist)
+myLinkedlist.prepend(1)
+// myLinkedlist.insert(2, 99)
+myLinkedlist.printList()
 
-// outputs: 
+
+// append outputs: 
 /* LinkedList {
 head: { value: 10, next: { value: 5, next: [Object] } },
 tail: { value: 16, next: null },
 length: 3
 }
 */
+
+/* prepend output:
+LinkedList {
+  head: { value: 1, next: { value: 10, next: [Object] } },
+  tail: { value: 16, next: null },
+  length: 4
+}
+ */
+
