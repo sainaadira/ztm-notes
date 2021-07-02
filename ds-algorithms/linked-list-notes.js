ba635fe711614c48588f4c9c028676f61615fdb1
Sai <snaadira@gmail.com>
2021-07-02 13:59:12 -0400

    added reverse method on singly linked list

--- ds-algorithms/linked-list-notes.js
+++ ds-algorithms/linked-list-notes.js
@@ -326,3 +326,156 @@ myLinkedlist.prepend(1)
 myLinkedlist.insert(1, 99)
 myLinkedlist.remove(2)
 myLinkedlist.printList()
+
+/*******************************************************
+            REVERSE A SINGLY LINKED LIST
+*******************************************************/
+
+
+class LinkedList {
+  // creates head or very first node
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head
+    //this is optional
+    this.length = 1
+  }
+  append(value) {
+    //creates new node with properies in constructor
+    const newNode = {
+      value: value,
+      next: null
+    }
+    // graps the tail and the pointer of the tail and points to new created node
+    this.tail.next = newNode
+    // the tail is now going to equal the new node
+    this.tail = newNode
+    this.length++
+    return this
+  }
+  prepend(value) {
+    // creating an empty node with the value
+    const newNode = {
+      value: value,
+      next: null
+    }
+    // new node with pointer to next node is now the head node
+    newNode.next = this.head
+    // the head is equal to newly created node (firstNode)
+    this.head = newNode
+    // increment length by one
+    this.length++
+    // return linked list
+    return this
+  }
+
+  printList() {
+    const array = []
+    // current node is the head node in the list
+    let currentNode = this.head
+    //while loop: while condition is happening, run below commands
+    // as long as there is a current node that doesnt point to null
+    while (currentNode !== null) {
+      // add to array the value of the current node
+      array.push(currentNode.value)
+      // then update the current node to equal next until null is reached
+      currentNode = currentNode.next
+    }
+    return array
+  }
+  insert(index, value) {
+    //  make sure its an index is a value 
+    // that is understood: if index is greater than or equal to length
+    if (index >= this.length) {
+      // append value to the end of the list
+      return this.append(value)
+    }
+    // creating new node
+    const newNode = {
+      value: value,
+      next: null
+    }
+    // first node: new method that gets last index 
+    const leaderNode = this.traverseToIndex(index - 1)
+    //grabs the next item in list: makes sure connections work properly -- 5 is referenced in holding pointer variable
+    const holdingPointer = leaderNode.next
+    // leader can now point to the new node rend remove reference to 5
+    leaderNode.next = newNode
+    // new node now equals 5
+    newNode.next = holdingPointer
+    // added to the list increments by 1
+    this.length++
+    // print the list
+    return this.printList()
+  }
+  traverseToIndex(index) {
+    let counter = 0
+    // current node is the head
+    let currentNode = this.head
+    // while counter does not equal the index
+    // keep traversing from head til counter is equal to the index
+    while (counter !== index) {
+      // current node moves over to the right
+      currentNode = currentNode.next
+      // increment counter one by one
+      counter++
+    }
+    // return current node
+    return currentNode
+  }
+
+  remove(index) {
+    // grab the leader
+    const leaderNode = this.traverseToIndex(index - 1)
+    // reference to the unwanted node
+    const nodeToDelete = leaderNode.next
+    // leader node is now the node to be deleted
+    leaderNode.next = nodeToDelete.next
+    // decrement length by 1 since item is being removed
+    this.length--
+    return this.printList()
+  }
+  // reference arr: [ 1, 10, 16, 88 ]
+  reverse() {
+    // checking to see if there is more than one item in the list
+    if (!this.head.next) {
+      // return the head node
+      return this.head
+    }
+    // first node is the head (1)
+    let first = this.head
+    let tail = this.head
+    // second node im the list (10)
+    let second = first.next
+    // as long as this second node exists and not null
+    while (second) {
+      // create temp variable that holds the third element (16)
+      const temp = second.next
+      // temp element points to the first
+      second.next = first
+      // first now ewuals the second element
+      first = second
+      // second equals the temp (third) element
+      second = temp
+    }
+    // 1 now points to null
+    this.head.next = null
+    //head becomes 88
+    this.head = first
+    return this
+  }
+}
+
+const myLinkedlist = new LinkedList(10)
+myLinkedlist.append(5)
+myLinkedlist.append(16)
+myLinkedlist.prepend(1)
+myLinkedlist.insert(2, 99)
+myLinkedlist.insert(20, 88)
+myLinkedlist.remove(2)
+myLinkedlist.remove(2)
+myLinkedlist.printList()
+myLinkedlist.reverse()
