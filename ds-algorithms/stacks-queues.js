d47db49491ed5334c32f26bd4f1fce9e1b277ae3
Sai <snaadira@gmail.com>
2021-07-24 22:29:15 -0400

    added more notes

--- ds-algorithms/stacks-queues.js
+++ ds-algorithms/stacks-queues.js
@@ -122,3 +122,69 @@ myStack.pop();
 
 
 
+
+/*********************************/
+//       QUEUE
+/*********************************/
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first
+  }
+  enqueue(value) {
+    const newNode = new Node(value)
+    if (this.length === 0) {
+      this.first = newNode
+      this.last = newNode
+    } else {
+      // whatever is the last value of the line and next
+      this.last.next = newNode
+      // new node is now last
+      this.last = newNode
+    }
+    this.length++
+    return this
+  }
+  dequeue() {
+    if (!this.first) {
+      return null
+    }
+    if (this.first === this.last) {
+      this.last = null
+    }
+    // would hold joy's name
+    const holdingPointer = this.first
+    this.first = this.first.next
+    this.length--
+    return this
+  }
+  //isEmpty;
+}
+
+const myQueue = new Queue();
+myQueue.enqueue('joy')
+myQueue.enqueue('matt')
+myQueue.enqueue('pavel')
+myQueue.enqueue('samir')
+
+
+//Joy
+//Matt
+//Pavel
+//Samir
+
+
+
