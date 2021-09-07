42f97df9c9f542286fe56bb245dc195125987724
Sai <snaadira@gmail.com>
2021-09-07 07:05:47 -0400

    graph and tree taversal notes and examples

--- ds-algorithms/algorithms/graph_tree_traversal.js
+++ ds-algorithms/algorithms/graph_tree_traversal.js
@@ -260,24 +260,12 @@ tree.breadthFirstSearch()
 tree.breadthFirstSearchR([tree.root], [])
 
 
-
-
-
 function traverse(node) {
   const tree = { value: node.value };
   tree.left = node.left === null ? null : traverse(node.left);
   tree.right = node.right === null ? null : traverse(node.right);
   return tree;
 }
-
-
-
-
-
-
-
-
-
 
 
 /************************************************ */
